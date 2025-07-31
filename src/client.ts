f3bce515b5f61fefae26a5e1c3ec11dbd947f1b4
Andrei <a.petrisor@studenti.unibg.it>
2025-07-31 17:31:14 +0200

    prompt and query implementation

--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,15 @@
-import { input, select } from "@inquirer/prompts";
+import "dotenv/config"
+import { input, select , confirm} from "@inquirer/prompts";
 import { Client } from "@modelcontextprotocol/sdk/client/index.js";
 import { StdioClientTransport } from "@modelcontextprotocol/sdk/client/stdio.js";
-import { Tool } from "@modelcontextprotocol/sdk/types.js";
+import { createOpenAI } from '@ai-sdk/openai';
+import { generateText, jsonSchema, ToolSet } from "ai" // or wherever `generateText` comes from
+import {
+  CreateMessageRequestSchema,
+  Prompt,
+  PromptMessage,
+  Tool,
+} from "@modelcontextprotocol/sdk/types.js"
 
 const mcp = new Client(
   {
@@ -19,6 +27,11 @@ const transport = new StdioClientTransport({
   stderr: "ignore",
 });
 
+const openai = createOpenAI({
+  apiKey: process.env.OPENAI_API_KEY, // or hardcode if testing: "sk-..."
+});
+
+
 async function main() {
   await mcp.connect(transport);
   const [{ tools }, { prompts }, { resources }, { resourceTemplates }] =
@@ -28,6 +41,24 @@ async function main() {
       mcp.listResources(),
       mcp.listResourceTemplates(),
     ]);
+
+     mcp.setRequestHandler(CreateMessageRequestSchema, async request => {
+    const texts: string[] = []
+    for (const message of request.params.messages) {
+      const text = await handleServerMessagePrompt(message)
+      if (text != null) texts.push(text)
+    }
+
+    return {
+      role: "user",
+      model: "gpt-3.5-turbo",
+      stopReason: "endTurn",
+      content: {
+        type: "text",
+        text: texts.join("\n"),
+      },
+    }
+  })
 
   console.log("You are connected");
   while (true) {
@@ -52,33 +83,50 @@ async function main() {
           await handleTool(tool);
         }
         break;
-       case "Resources":
+      case "Resources":
         const resourceUri = await select({
           message: "Select a resource",
           choices: [
-            ...resources.map(resource => ({
+            ...resources.map((resource) => ({
               name: resource.name,
               value: resource.uri,
               description: resource.description,
             })),
-            ...resourceTemplates.map(template => ({
+            ...resourceTemplates.map((template) => ({
               name: template.name,
               value: template.uriTemplate,
               description: template.description,
             })),
           ],
-        })
+        });
         const uri =
-          resources.find(r => r.uri === resourceUri)?.uri ??
-          resourceTemplates.find(r => r.uriTemplate === resourceUri)
-            ?.uriTemplate
+          resources.find((r) => r.uri === resourceUri)?.uri ??
+          resourceTemplates.find((r) => r.uriTemplate === resourceUri)
+            ?.uriTemplate;
         if (uri == null) {
-          console.error("Resource not found.")
+          console.error("Resource not found.");
         } else {
-          await handleResource(uri)
+          await handleResource(uri);
         }
-        break
-        
+        break;
+      case "Prompts":
+        const promptName = await select({
+          message: "Select a prompt",
+          choices: prompts.map((prompt) => ({
+            name: prompt.name,
+            value: prompt.name,
+            description: prompt.description,
+          })),
+        });
+        const prompt = prompts.find((p) => p.name === promptName);
+        if (prompt == null) {
+          console.error("Prompt not found.");
+        } else {
+          await handlePrompt(prompt);
+        }
+        break;
+      case "Query":
+        await handleQuery(tools);
     }
   }
 }
@@ -101,26 +149,93 @@ async function handleTool(tool: Tool) {
 }
 
 async function handleResource(uri: string) {
-  let finalUri = uri
-  const paramMatches = uri.match(/{([^}]+)}/g) //matches what the content beetween 2 brakets like {}
+  let finalUri = uri;
+  const paramMatches = uri.match(/{([^}]+)}/g); //matches what the content beetween 2 brakets like {}
 
   if (paramMatches != null) {
     for (const paramMatch of paramMatches) {
-      const paramName = paramMatch.replace("{", "").replace("}", "")
+      const paramName = paramMatch.replace("{", "").replace("}", "");
       const paramValue = await input({
         message: `Enter value for ${paramName}:`,
-      })
-      finalUri = finalUri.replace(paramMatch, paramValue)
+      });
+      finalUri = finalUri.replace(paramMatch, paramValue);
     }
   }
 
   const res = await mcp.readResource({
     uri: finalUri,
-  })
+  });
 
   console.log(
     JSON.stringify(JSON.parse(res.contents[0].text as string), null, 2)
+  );
+}
+
+async function handlePrompt(prompt: Prompt) {
+  const args: Record<string, string> = {};
+  for (const arg of prompt.arguments ?? []) {
+    args[arg.name] = await input({
+      message: `Enter value for ${arg.name}:`,
+    });
+  }
+
+  const response = await mcp.getPrompt({
+    name: prompt.name,
+    arguments: args,
+  });
+
+  for (const message of response.messages) {
+    console.log(await handleServerMessagePrompt(message));
+  }
+}
+
+async function handleQuery(tools: Tool[]) {
+  const query = await input({ message: "Enter your query" })
+
+  const { text, toolResults } = await generateText({
+    model: openai('gpt-3.5-turbo'),
+    prompt: query,
+    tools: tools.reduce(
+      (obj, tool) => ({
+        ...obj,
+        [tool.name]: {
+          description: tool.description,
+          parameters: jsonSchema(tool.inputSchema),
+          execute: async (args: Record<string, any>) => {
+            return await mcp.callTool({
+              name: tool.name,
+              arguments: args,
+            })
+          },
+        },
+      }),
+      {} as ToolSet
+    ),
+  })
+
+  console.log(
+    // @ts-expect-error
+    text || toolResults[0]?.result?.content[0]?.text || "No text generated."
   )
+}
+
+async function handleServerMessagePrompt(message: PromptMessage) {
+  if (message.content.type !== "text") return;
+
+  console.log(message.content.text);
+  const run = await confirm({
+    message: "Would you like to run the above prompt",
+    default: true,
+  });
+
+  if (!run) return;
+
+  const { text } = await generateText({
+    model: openai('gpt-3.5-turbo'),
+    prompt: message.content.text,
+  });
+
+  return text;
 }
 
 
